88eff316d57ee89100c3bd5eaf4f764446e631fe
DTH2356763-NguyenHoangKhang <130205khang@gmail.com>
2025-09-18 13:38:50 +0700

    Add bai tap buoi TH3

---
+++ BuoiTH3_Bai1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BuoiTH3_Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.chkNho = new System.Windows.Forms.CheckBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnDung = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ten dang nhap: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mat khau: ";
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.Info;
+            this.txtPass.Location = new System.Drawing.Point(187, 125);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(262, 20);
+            this.txtPass.TabIndex = 2;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.SystemColors.Info;
+            this.txtUser.Location = new System.Drawing.Point(188, 69);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(262, 20);
+            this.txtUser.TabIndex = 3;
+            // 
+            // chkNho
+            // 
+            this.chkNho.AutoSize = true;
+            this.chkNho.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkNho.Location = new System.Drawing.Point(88, 169);
+            this.chkNho.Name = "chkNho";
+            this.chkNho.Size = new System.Drawing.Size(84, 24);
+            this.chkNho.TabIndex = 4;
+            this.chkNho.Text = "Ghi nho";
+            this.chkNho.UseVisualStyleBackColor = true;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(53, 210);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(119, 31);
+            this.btnDangNhap.TabIndex = 5;
+            this.btnDangNhap.Text = "Dang nhap";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // btnDung
+            // 
+            this.btnDung.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDung.Location = new System.Drawing.Point(343, 210);
+            this.btnDung.Name = "btnDung";
+            this.btnDung.Size = new System.Drawing.Size(119, 31);
+            this.btnDung.TabIndex = 6;
+            this.btnDung.Text = "Dung";
+            this.btnDung.UseVisualStyleBackColor = true;
+            this.btnDung.Click += new System.EventHandler(this.btnDung_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(199, 210);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(119, 31);
+            this.btnXoa.TabIndex = 7;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 313);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnDung);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.chkNho);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form Dang Nhap";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.CheckBox chkNho;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnDung;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
---
+++ BuoiTH3_Bai1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuoiTH3_Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.txtUser.Focus();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string thongbao;
+            thongbao = "Ten dang nhap la ";
+            thongbao += this.txtUser.Text;
+            thongbao += "\n\rMat khau la ";
+            thongbao += this.txtPass.Text;
+            if(this.chkNho.Checked == true) 
+                thongbao += "\n\rBan co ghi nho.";
+            MessageBox.Show(thongbao, "Thong bao");
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.txtUser.Clear();
+            this.txtPass.Clear();
+            this.chkNho.Checked = false;
+            this.txtUser.Focus();
+        }
+
+        private void btnDung_Click(object sender, EventArgs e)
+        {
+            DialogResult traloi;
+            traloi=MessageBox.Show("Ban co muon dong chuong trinh khong?","Thong bao",MessageBoxButtons.OKCancel,MessageBoxIcon.Question);
+            if(traloi == DialogResult.OK)
+                 Application.Exit();
+        }
+    }
+}
