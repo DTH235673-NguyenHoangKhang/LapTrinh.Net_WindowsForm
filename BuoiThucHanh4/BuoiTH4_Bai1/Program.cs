984320ce57d9c6d1ff981d3f92a789f69ac18640
DTH235673-NguyenHoangKhang <130205khang@gmail.gmailcom>
2025-10-04 20:27:00 +0700

    Add bai tap buoi thuc hanh 4

--- BuoiThucHanh4/BuoiTH4_Bai1/Program.cs
+++ BuoiThucHanh4/BuoiTH4_Bai1/Program.cs
@@ -17,6 +17,7 @@ namespace BuoiTH4_Bai1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
